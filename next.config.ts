e583c6658bf0ea2478412957d5929d8125bf201f
rzfann <fajrulsyaban47@gmail.com>
2025-12-17 21:20:14 +0700

    fix pathname

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+
    images: {
+    domains: ['c4.wallpaperflare.com'],
     remotePatterns: [
       {
         protocol: 'https',
